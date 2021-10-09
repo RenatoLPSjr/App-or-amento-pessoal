0e180c102f13299efe20127dff9085f248330924
RenatoLPSjr <renato.lop.jr@gmail.com>
2021-10-09 19:01:06 -0300

    Adicionando o cadastro de despesas

---
+++ app.js
@@ -0,0 +1,11 @@
+function cadastrarDespesas() {
+
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    console.log(ano)
+}
